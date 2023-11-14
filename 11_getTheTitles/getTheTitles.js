5a364eea4047a10ae4fff33aaeea8c42e06fbea7
JCtech <ocjustc@gmail.com>
2023-11-14 14:19:52 +0100

    Worked with array methods. Need to check up 'reduce()' method.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
+function getTheTitles(arr) {
+  return arr.map(item => item.title)
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+//Unable to solve this problem at the moment.
+// Need to understand reduce() method first.
 
+const findTheOldest = function(arr) {
+  const year = new Date().getFullYear();
+
+  const oldest = arr.map((item) => {if (!(item.hasOwnProperty('yearOfDeath'))) {item.yearOfDeath = year;}
+  item.yearOfDeath - item.yearOfBirth});
+
+  //oldest.sort((a, b) => b - a);
+  //return oldest[0];
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+//module.exports = findTheOldest;
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
